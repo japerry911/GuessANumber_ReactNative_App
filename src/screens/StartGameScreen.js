95a034a3e6250c5c9bdee3a070fa15c50c091c61
japerry911 <jack.e.perry@outlook.com>
2020-04-02 22:01:43 -0600

    Basic containers for StartGameScreen.js

--- src/screens/StartGameScreen.js
+++ src/screens/StartGameScreen.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 
 const StartGameScreen = () => {
     return (
-        <View styles={styles.screenStyle}>
-            <Text>Game Screen!</Text>
+        <View style={styles.screenStyle}>
+            <Text style={styles.titleStyle}>Start a New Game!</Text>
+            <View style={styles.inputViewStyle}>
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonViewStyle}>
+                    <Button title='Reset' />
+                    <Button title='Confirm' />
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    inputViewStyle: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    titleStyle: {
+        fontSize: 20,
+        marginVertical: 10
+    },
     screenStyle: {
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    buttonViewStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        width: '100%',
+        paddingHorizontal: 15
     }
 });
 
