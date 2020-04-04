ec8757d6dac1bc1384056b9e24a08849cdb4b194
japerry911 <jack.e.perry@outlook.com>
2020-04-03 21:14:58 -0600

    Initial Boilerplate setup for MainButton

---
+++ src/components/MainButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const MainButton = props => {};
+
+const styles = StyleSheet.create({});
+
+export default MainButton;
